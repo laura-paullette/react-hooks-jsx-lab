879d4a1adab5afcacafffb9b497a60f94a4e00c5
laura-paullette <laura.paullette@student.moringaschool.com>
2023-04-19 17:23:24 +0300

    completed assignment

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,13 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+  <div id="about">
+    <h1>About Me</h1>
+    <p>I love travelling </p>
+    <img src={image} alt="Imade this"/>
+  </div>
+  );
 }
 
 export default About;
